130a7f74b36b82edb2d5ec68cdf289f1c1024dbe
CEMAL NISAN <cnisan21@ku.edu.tr>
2025-05-01 22:03:04 +0000

    updated_v03_beforeFineTunning

--- src/model.py
+++ src/model.py
@@ -2,24 +2,43 @@ from torch import nn
 import torch.nn.functional as F
 import torch.nn as nn
 import torchvision.models as models
+import torch
+from options import args_parser
+args = args_parser()
+
 
 class CNN(nn.Module):
     def __init__(self, args):
         super(CNN, self).__init__()
+        self.args = args
         self.conv1 = nn.Conv2d(args.num_channels, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
+
+
+        # Dummy forward pass to calculate the flattened size
+        self._to_linear = None
+        self._init_linear_layer()
+
+        self.fc1 = nn.Linear(self._to_linear, 50)
         self.fc2 = nn.Linear(50, args.num_classes)
+    
+    def _init_linear_layer(self):
+        # Dummy forward pass to calculate the flattened size
+        x = torch.randn(1, self.args.num_channels, 224, 224)
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2_drop(self.conv2(x))), 2)
+        self._to_linear = x.view(1, -1).shape[1]
+
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
+        x = x.view(-1, self._to_linear)
         x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
         x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        return x
+
     
 def get_model(model_name, num_classes):
     """
@@ -34,7 +53,7 @@ def get_model(model_name, num_classes):
         nn.Module: the model
     """
     if model_name == 'cnn':
-        return CNN(num_classes)  # mevcut basit CNN modelin
+        return CNN(args)  # mevcut basit CNN modelin
     elif model_name == 'resnet18':
         model = models.resnet18(pretrained=True)
         model.fc = nn.Linear(model.fc.in_features, num_classes)
